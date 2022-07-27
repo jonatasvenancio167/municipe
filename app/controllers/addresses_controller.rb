f3fc88a420391d2a197ddc40f8949ce33b7a5e46
~jonatasvenancio167~ <~jvenancio167@hotmail.com~>
2022-07-27 13:23:37 -0300

    create address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,14 +7,12 @@ class AddressesController < ApplicationController
   end
 
   def create
-    @address = Address.new(object_params)
+    @address = Address.new(address_params)
 
     respond_to do |format|
       if @address.save
-        format.html { redirect_to object_url(@address), notice: I18n.t("address.address_was_successfully_created") }
+        format.html { redirect_to address_url(@address), notice: "address was successfully created." }
         format.json { render :show, status: :created, location: @address }
-
-        respond_with @address, location: { action: 'index' }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @address.errors, status: :unprocessable_entity }
@@ -23,22 +21,21 @@ class AddressesController < ApplicationController
   end
 
   def update
-    puts "address --->#{params[:city]}"
-    if @address.update(object_params)
-
-      # @object.image.attach(params[@object.class.name.underscore][:image]) if object_params.has_key?(:image)
-      # @object.bw_image.attach(params[@object.class.name.underscore][:bw_image]) if object_params.has_key?(:bw_image)
-
-      respond_to @address, location: { action: 'index' }
-    else
-      respond_with @address
+    respond_to do |format|
+      if @address.update(address_params)
+        format.html { redirect_to address_url(@address), notice: "address was successfully updated." }
+        format.json { render :show, status: :ok, location: @address }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @address.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-    
+
     private
 
     def address_params
-      params.require(:address).permit(:cep, :public_place, :complement, :neighborhood, :city, :uf, :ibge_code, :municipe_id)
+      params.require(:address).permit(:id, :cep, :public_place, :complement, :neighborhood, :city, :uf, :ibge_code, :municipe_id)
     end
 end
