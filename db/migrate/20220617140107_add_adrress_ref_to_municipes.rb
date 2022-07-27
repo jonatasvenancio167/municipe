aaae33e848e368341259801b055565031fd6ea65
~jonatasvenancio167~ <~jvenancio167@hotmail.com~>
2022-07-27 13:52:57 -0300

    add migrations

--- db/migrate/20220617140107_add_adrress_ref_to_municipes.rb
+++
@@ -1,5 +0,0 @@
-class AddAdrressRefToMunicipes < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :municipes, :address, foreign_key: true
-  end
-end
--- db/migrate/20220621113946_add_municipeto_municipes.rb
+++
@@ -1,4 +0,0 @@
-class AddMunicipetoMunicipes < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
