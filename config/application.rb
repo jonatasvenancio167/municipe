a73c34c34c0e59e36393a102b10ec64873f3045a
~jonatasvenancio167~ <~jvenancio167@hotmail.com~>
2022-07-27 13:39:43 -0300

    config smtp to send email

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,24 @@ module App
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.test_driver = :selenium_chrome
+
     config.i18n.available_locales = ['pt-BR']
     config.i18n.default_locale = "pt-BR"
     config.time_zone = 'America/Sao_Paulo'
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      domain:               'smpt.gmail.com',
+      port:                 587,
+      user_name:            'jvenancio649@gmail.com',
+      password:             'sxjxincqlkszpzol',
+      authentication:       'plain',
+      enable_starttls_auto: true,
+    }
+
   end
 end
